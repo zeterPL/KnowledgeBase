226e2e3842f31210d23d0b56727e787c5ed7f1ca
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-07-03 11:53:24 +0200

    resource model

--- KnowledgeBase.Data/Models/Resource.cs
+++ KnowledgeBase.Data/Models/Resource.cs
@@ -2,5 +2,9 @@
 
 public class Resource
 {
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public Project Project { get; set; }
+    public ResourceCategory Category { get; set; }
 }
 

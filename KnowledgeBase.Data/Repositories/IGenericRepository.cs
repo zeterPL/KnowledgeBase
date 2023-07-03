0c285352675b2ba459df94ed8fee787be5a5ed8c
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-07-03 13:58:38 +0200

    GenericRepo interface

---
+++ KnowledgeBase.Data/Repositories/IGenericRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace KnowledgeBase.Data.Repositories;
+
+public interface IGenericRepository<T>
+{
+    public void Add(T entity);
+    public T Get(Guid id);
+    public void Remove(T entity);
+    public void Update(T entity);
+}

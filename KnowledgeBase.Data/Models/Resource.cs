7c49b9e603a3255cba653e91c679faaff468c8a2
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-07-03 11:56:44 +0200

    id prop in resource model

--- KnowledgeBase.Data/Models/Resource.cs
+++ KnowledgeBase.Data/Models/Resource.cs
@@ -2,6 +2,7 @@
 
 public class Resource
 {
+    public Guid Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public Project Project { get; set; }

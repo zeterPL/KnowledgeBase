626361b02c100ce74d7567484b758797be6c3416
PiotrDąbrowski <piotr.dabrowski2001@o2.pl>
2023-07-04 10:22:03 +0200

    Controller Added

---
+++ KnowledgeBase.Web/Pages/Resource/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KnowledgeBase.Web.Pages.Resource
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

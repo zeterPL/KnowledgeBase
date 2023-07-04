e39a1044682bcb2f72f3bef13b4b9544cacbf4ef
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-07-04 11:41:21 +0200

    mvc web project

--- KnowledgeBase.Web/Program.cs
+++ KnowledgeBase.Web/Program.cs
@@ -1,6 +1,7 @@
 using KnowledgeBase.Data.Data;
 using KnowledgeBase.Data.Models;
-using Microsoft.AspNetCore.Identity;
+using KnowledgeBase.Data.Repositories;
+using KnowledgeBase.Logic.Services;
 using Microsoft.EntityFrameworkCore;
 
 
@@ -9,26 +10,30 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<KnowledgeDbContext>(options =>
-    options.UseSqlServer(connectionString));
+	options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<KnowledgeDbContext>();
+	.AddEntityFrameworkStores<KnowledgeDbContext>();
 
-builder.Services.AddRazorPages();
+// Dependency injection
+builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
+builder.Services.AddScoped<IProjectService, ProjectService>();
+
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseMigrationsEndPoint();
+	app.UseMigrationsEndPoint();
 }
 else
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -40,5 +45,9 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller}/{action=Index}/{id?}");
 
 app.Run();

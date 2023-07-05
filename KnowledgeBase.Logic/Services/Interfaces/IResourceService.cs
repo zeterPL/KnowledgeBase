6fe091389618f5ee908d03c46333c31816693844
Piotr Dąbrowski <106407699+PiotrDabrowski232@users.noreply.github.com>
2023-07-05 14:58:40 +0200

    white spaces IResourceService.cs

--- KnowledgeBase.Logic/Services/Interfaces/IResourceService.cs
+++ KnowledgeBase.Logic/Services/Interfaces/IResourceService.cs
@@ -12,8 +12,8 @@ namespace KnowledgeBase.Logic.Services.Interfaces
         public void Add(Resource entity);
         public Resource Get(Guid id);
         public void Remove(Resource entity);
-		public void Deleted(Resource entity);
-		public void Update(Resource entity);
+	public void Deleted(Resource entity);
+	public void Update(Resource entity);
         public IEnumerable<Resource> GetAll();
     }
 }

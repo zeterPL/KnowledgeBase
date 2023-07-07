74b596fee2d63349f431bd488cbe8d4687e4572f
zeterPL <ukaszjanowicz@gmail.com>
2023-07-07 11:37:32 +0200

    add user

---
+++ KnowledgeBase.Logic/Dto/UserDto.cs
@@ -0,0 +1,34 @@
+﻿using KnowledgeBase.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeBase.Logic.Dto
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }    
+    }
+
+    public static class UserExtensions
+    {
+        public static UserDto ToUserDto(this User user)
+        {
+            return new UserDto
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                UserName = user.UserName,
+            };
+        }
+    }
+}

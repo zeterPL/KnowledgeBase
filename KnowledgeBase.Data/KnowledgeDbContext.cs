5741ed4feb37f0e3630ce864beb50f59d31af2c3
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-07-09 11:44:17 +0200

    using assembly for entities configuration

---
+++ KnowledgeBase.Data/KnowledgeDbContext.cs
@@ -0,0 +1,18 @@
+﻿using KnowledgeBase.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace KnowledgeBase.Data;
+
+public class KnowledgeDbContext : IdentityDbContext<User, Role, Guid>
+{
+    public KnowledgeDbContext(DbContextOptions<KnowledgeDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(typeof(KnowledgeDbContext).Assembly);
+    }
+}

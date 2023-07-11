b3140bedd2de03bc84c977214381283b76841be6
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-07-11 19:14:33 +0200

    upload file in ResourceDto

--- KnowledgeBase.Logic/Dto/ResourceDto.cs
+++ KnowledgeBase.Logic/Dto/ResourceDto.cs
@@ -1,15 +1,20 @@
 ﻿using KnowledgeBase.Data.Models.Enums;
+using Microsoft.AspNetCore.Http;
 
 namespace KnowledgeBase.Logic.Dto
 {
     public class ResourceDto
     {
-		public Guid Id { get; set; }
-		public string Name { get; set; }
-		public string Description { get; set; }
-		public Guid ProjectId { get; set; }
-		public virtual ResourceCategory Category { get; set; }
-		public Guid UserId { get; set; }
-		public bool IsDeleted { get; set; }
-	}
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Guid ProjectId { get; set; }
+        public virtual ResourceCategory Category { get; set; }
+        public Guid UserId { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public string? AzureStorageAbsolutePath { get; set; }
+        public string? AzureFileName { get; set; }
+        public IFormFile File { get; set; }
+    }
 }

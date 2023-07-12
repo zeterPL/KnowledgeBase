4e56278b53f7f8bb9c2b7a6bff8324945c235c87
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-07-12 15:05:35 +0200

    upload and download in resource controller

--- KnowledgeBase.Web/Controllers/ResourceController.cs
+++ KnowledgeBase.Web/Controllers/ResourceController.cs
@@ -1,50 +1,28 @@
 ﻿using Azure;
-using KnowledgeBase.Logic.AzureServices;
-using KnowledgeBase.Logic.AzureServices.File;
 using KnowledgeBase.Logic.Dto;
 using KnowledgeBase.Logic.Services.Interfaces;
 using KnowledgeBase.Shared;
 using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
 
 namespace KnowledgeBase.Web.Controllers
 {
     public class ResourceController : Controller
     {
-        private readonly IResourceService _service;
-        private readonly AzureStorageService _azureStorageService;
-        private readonly IProjectService _projectService;
+        private readonly IResourceService _resourceService;
 
-        public ResourceController(IResourceService service, AzureStorageService azureStorageService, IProjectService projectService)
+        public ResourceController(IResourceService resourceService)
         {
-            _service = service;
-            _azureStorageService = azureStorageService;
-            _projectService = projectService;
+            _resourceService = resourceService;
         }
 
         public IActionResult Index()
         {
-            return View(_service.GetAll().ToList());
+            return View(_resourceService.GetAll().ToList());
         }
 
         public IActionResult Create()
         {
             return View();
-        }
-
-        private async Task<ResourceDto> UploadFile(ResourceDto resourceDto, string projectName)
-        {
-            if (resourceDto.File == null)
-            {
-                throw new ArgumentException("File cant be null");
-            }
-
-            var uploadFile = new UploadAzureResourceFile(resourceDto.Name, projectName, resourceDto.File);
-            var azureResourceFile = await _azureStorageService.UploadFileAsync(uploadFile);
-
-            resourceDto.AzureStorageAbsolutePath = azureResourceFile.AzureStoragePath;
-            resourceDto.AzureFileName = azureResourceFile.AzureFileName;
-            return resourceDto;
         }
 
         [HttpPost]
@@ -53,20 +31,14 @@ namespace KnowledgeBase.Web.Controllers
         {
             resourceDto.UserId = User.GetUserId();
             resourceDto.ProjectId = Guid.Parse("8f94efce-fa7a-47d8-98e6-08db7ede4d7b");
-            var projectName = _projectService.Get(resourceDto.ProjectId)?.Name;
-            if (projectName == null)
-            {
-                return BadRequest();
-            }
 
             try
             {
-                resourceDto = await UploadFile(resourceDto, projectName);
-                _service.Add(resourceDto);
+                await _resourceService.AddAsync(resourceDto);
             }
-            catch(ArgumentException ex)
+            catch (ArgumentException)
             {
-                return BadRequest(ex.Message);
+                return BadRequest();
             }
             catch (RequestFailedException ex)
             {
@@ -84,34 +56,17 @@ namespace KnowledgeBase.Web.Controllers
             {
                 return View(resourceDto);
             }
-            resourceDto.UserId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            resourceDto.UserId = User.GetUserId();
             resourceDto.ProjectId = Guid.Parse("8f94efce-fa7a-47d8-98e6-08db7ede4d7b");
 
-            var resource = _service.Get(resourceDto.Id.ToGuid());
-            if (resource == null)
-            {
-                return NotFound();
-            }
-
-            if (resourceDto.File == null)
-            {
-                _service.Update(resourceDto);
-                return RedirectToAction(actionName: "Index");
-            }
-
-            var projectName = _projectService.Get(resourceDto.ProjectId)?.Name;
-            if (projectName == null)
-            {
-                return BadRequest();
-            }
             try
             {
-                resourceDto = await UploadFile(resourceDto, projectName);
-                _service.Update(resourceDto);
+                await _resourceService.UpdateAsync(resourceDto);
             }
-            catch (ArgumentException ex)
+            catch (ArgumentException)
             {
-                return BadRequest(ex.Message);
+                return BadRequest();
             }
             catch (RequestFailedException ex)
             {
@@ -123,54 +78,38 @@ namespace KnowledgeBase.Web.Controllers
 
         public IActionResult Edit(Guid id)
         {
-            var resource = _service.Get(id);
+            var resource = _resourceService.Get(id);
             if (resource == null)
             {
                 return NotFound();
             }
+
             return View(resource);
         }
 
         public IActionResult Delete(Guid id)
         {
-            return View(_service.Get(id));
+            return View(_resourceService.Get(id));
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Delete(ResourceDto resourcedto)
+        public IActionResult Delete(ResourceDto resourceDto)
         {
-            _service.SoftDelete(resourcedto);
+            _resourceService.SoftDelete(resourceDto);
             return RedirectToAction(actionName: "Index");
         }
 
         [HttpGet]
         public async Task<IActionResult> Download(Guid id)
         {
-            var resource = _service.Get(id);
+            var resource = await _resourceService.DownloadAsync(id);
             if (resource == null)
             {
                 return NotFound();
             }
 
-            var fileDto = new AzureResourceFile
-            {
-                AzureStoragePath = resource.AzureStorageAbsolutePath,
-            };
-
-            try
-            {
-                var file = await _azureStorageService.DownloadFileAsync(fileDto);
-                return File(file.Stream, file.ContentType, resource.AzureFileName);
-            }
-            catch (FileNotFoundException)
-            {
-                return NotFound();
-            }
-            catch (RequestFailedException ex)
-            {
-                return StatusCode(ex.Status);
-            }
+            return File(resource.Content, resource.ContentType, resource.AzureFileName);
         }
     }
 }

048a1b45bfbfc463593bc5454903d9db889cd1ee
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-07-12 14:59:16 +0200

    resource repository get resource include project

--- KnowledgeBase.Data/Repositories/ResourceRepository.cs
+++ KnowledgeBase.Data/Repositories/ResourceRepository.cs
@@ -1,18 +1,27 @@
-﻿using KnowledgeBase.Data;
-using KnowledgeBase.Data.Models;
+﻿using KnowledgeBase.Data.Models;
 using KnowledgeBase.Data.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace KnowledgeBase.Data.Repositories;
 
-public class ResourceRepository : GenericRepository<Resource>, IGenericRepository<Resource>, IResourceRepository
+public class ResourceRepository : GenericRepository<Resource>, IResourceRepository
 {
     public ResourceRepository(KnowledgeDbContext context) : base(context)
     {
     }
 
-    public void Delete(Resource resource)
+    public void SoftDelete(Resource resource)
     {
         resource.IsDeleted = true;
         Update(resource);
+    }
+
+    public async Task<Resource?> GetResourceWithProjectAsync(Guid id)
+    {
+        var set = GetSet();
+        var resource = await set.Include(r => r.Project)
+            .Where(r => !r.IsDeleted)
+            .SingleAsync(r => r.Id == id);
+        return resource;
     }
 }

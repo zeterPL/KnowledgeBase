36b83b609d4d2690253347d00cf3a0f05e1ceeeb
zeterPL <ukaszjanowicz@gmail.com>
2023-07-13 11:55:07 +0200

    connection setup

--- KnowledgeBase.Web/Program.cs
+++ KnowledgeBase.Web/Program.cs
@@ -14,12 +14,10 @@ var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurre
 try
 {
 	var builder = WebApplication.CreateBuilder(args);
-
 	// Add services to the container.
 	var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 	builder.Services.AddDbContext<KnowledgeDbContext>(options =>
-		options.UseSqlServer(connectionString,
-			optionsSqlServer => { optionsSqlServer.MigrationsAssembly("KnowledgeBase.Data"); }));
+		options.UseSqlServer(connectionString));
 	builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 	builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)

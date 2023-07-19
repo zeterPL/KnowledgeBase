7ce4f86efa8c7cbdf9dc70ca9e166f12cde29d37
PiotrDąbrowski <piotr.dabrowski2001@o2.pl>
2023-07-19 12:03:08 +0200

    ResourceAPI

---
+++ KnowledgeBase.Web/Controllers/ApiControllers/ResourceApiController.cs
@@ -0,0 +1,49 @@
+﻿using KnowledgeBase.Logic.Dto;
+using KnowledgeBase.Logic.Services;
+using KnowledgeBase.Logic.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KnowledgeBase.Web.Controllers.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResourceApiController : ControllerBase
+    {
+        private readonly IResourceService _resourceService;
+        private readonly IProjectService _projectService;
+
+        public ResourceApiController(IResourceService resourceService, IProjectService projectService)
+        {
+            _resourceService = resourceService;
+            _projectService = projectService;
+        }
+
+        [Route("/ResourceController")]
+        [HttpGet]
+        public IEnumerable<ResourceDto> Get()
+        {
+            return _resourceService.GetAll();
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] ResourceDto resourcedto)
+        {
+            return Ok(_resourceService.AddAsync(resourcedto));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] ResourceDto resourcedto)
+        {
+            return Ok(_resourceService.UpdateAsync(resourcedto));
+        }
+
+        [HttpDelete]
+        [HttpPut]
+        public IActionResult Delete([FromBody] ResourceDto resourcedto)
+        {
+            _resourceService.SoftDelete(resourcedto);
+            return NoContent();
+        }
+    }
+}

5be8abf7b150b37dea3f87a00c094b6d418fc667
zeterPL <ukaszjanowicz@gmail.com>
2023-07-25 10:41:49 +0200

    index view

---
+++ KnowledgeBase.Web/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using KnowledgeBase.Web.Authorization;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KnowledgeBase.Web.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [Authorize(Policy = UserRolesTypes.SuperAdmin)]
+        public IActionResult AdminPanel()
+        {
+            return View();
+        }
+    }
+}

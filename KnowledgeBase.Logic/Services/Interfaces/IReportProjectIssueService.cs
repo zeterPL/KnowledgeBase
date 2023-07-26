5e802b837e6f422363f5b326070b12036022be44
zeterPL <ukaszjanowicz@gmail.com>
2023-07-26 12:19:25 +0200

    report service

---
+++ KnowledgeBase.Logic/Services/Interfaces/IReportProjectIssueService.cs
@@ -0,0 +1,21 @@
+﻿using KnowledgeBase.Logic.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeBase.Logic.Services.Interfaces
+{
+    public interface IReportProjectIssueService
+    {
+        public IList<ReportProjectIssueDto> GetAll();
+        public ReportProjectIssueDto Get(Guid id);
+        public IList<ReportProjectIssueDto> GeByProjectId(Guid projectId);
+        public void Create(ReportProjectIssueDto reportProjectIssueDto);
+        public void Close(Guid id);
+        public void ReOpen(Guid id);
+        public void Delete(Guid id);
+        public void Update(ReportProjectIssueDto reportDto);
+    }
+}
---
+++ KnowledgeBase.Logic/Services/ReportProjectIssueService.cs
@@ -0,0 +1,89 @@
+﻿using KnowledgeBase.Data.Models;
+using KnowledgeBase.Data.Repositories.Interfaces;
+using KnowledgeBase.Logic.Dto;
+using KnowledgeBase.Logic.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeBase.Logic.Services
+{
+    public class ReportProjectIssueService : IReportProjectIssueService
+    {
+        private readonly IReportProjectIssueRepository _reportRepository;
+
+        public ReportProjectIssueService(IReportProjectIssueRepository reportRepository)
+        {
+            _reportRepository = reportRepository;
+        }
+
+        private ReportProjectIssue toReportProjectIssue(ReportProjectIssueDto projectIssueDto)
+        {
+            return new ReportProjectIssue
+            {
+                Id = projectIssueDto.Id,
+                Title = projectIssueDto.Title,
+                Description = projectIssueDto.Description,
+                CreatedDate = projectIssueDto.CreatedDate,
+                IsOpen = projectIssueDto.IsOpen,
+                UserId = projectIssueDto.UserId,
+                ProjectId = projectIssueDto.ProjectId,
+            };
+        }
+
+        public void Close(Guid id)
+        {
+            var report = _reportRepository.Get(id);
+            report.IsOpen = false;
+            _reportRepository.Update(report);
+        }
+
+        public void Delete(Guid id)
+        {
+            var report = _reportRepository.Get(id);
+            _reportRepository.Remove(report);
+        }
+
+        public IList<ReportProjectIssueDto> GeByProjectId(Guid projectId)
+        {
+            var result = _reportRepository.GetAll().Where(x => x.ProjectId == projectId);
+            if (!result.Any()) return null;
+            else return result.Select(x => x.toReportProjectIssueDto()).ToList();
+        }
+
+        public ReportProjectIssueDto Get(Guid id)
+        {
+            var result = _reportRepository.Get(id);
+            if (result is null) return null;
+            else return result.toReportProjectIssueDto();
+        }
+
+        public IList<ReportProjectIssueDto> GetAll()
+        {
+            var result = _reportRepository.GetAll();
+            if (!result.Any()) return null;
+            else return result.Select(x => x.toReportProjectIssueDto()).ToList();
+        }
+
+        public void ReOpen(Guid id)
+        {
+            var result = _reportRepository.Get(id);
+            result.IsOpen = true;
+            _reportRepository.Update(result);
+        }
+
+        public void Update(ReportProjectIssueDto reportDto)
+        {
+            var tmp = toReportProjectIssue(reportDto);
+            _reportRepository.Update(tmp);
+        }
+
+        public void Create(ReportProjectIssueDto reportProjectIssueDto)
+        {
+            var tmp = toReportProjectIssue(reportProjectIssueDto);
+            _reportRepository.Add(tmp);
+        }
+    }
+}

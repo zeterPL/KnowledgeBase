b9617fe7d4c397cb9fb0a6646e3aa9dce1e84051
PiotrDąbrowski <piotr.dabrowski2001@o2.pl>
2023-07-27 16:51:16 +0200

    prepared schema to send email

--- Email/Function1.cs
+++ Email/Function1.cs
@@ -1,34 +1,37 @@
-using System;
-using System.Text;
-using System.Threading.Tasks;
 using Email.Models;
-using Microsoft.Azure.ServiceBus;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using SendGrid.Helpers.Mail;
+using System;
+using System.Net;
+using System.Net.Mail;
 
 namespace Email
 {
-    public class Function1
-    {
-        [FunctionName("Function1")]
-        public async Task Run([ServiceBusTrigger("email-queue", Connection = "connection")]string message, ILogger log)
-        {
+	public class Function1
+	{
+		[FunctionName("Function1")]
+		public void Run([ServiceBusTrigger("email-queue", Connection = "connection")] string message, ILogger log)
+		{
 
 			log.LogInformation($"C# ServiceBus queue trigger function processed message: {message}");
 
-            var messageBodyReceived = JsonConvert.DeserializeObject<MessageModel>(message);
+			var messageReceived = JsonConvert.DeserializeObject<MessageModel>(message);
 
-            Console.WriteLine(messageBodyReceived.PermissionsToString(messageBodyReceived.RequestedPermissions, messageBodyReceived.SenderName));
-            
-            var messageToSend = new SendGridMessage();
+			MailMessage mailMessage = new MailMessage();
+			mailMessage.From = new MailAddress("");
+			mailMessage.To.Add("");
+			mailMessage.Subject = "Permissions";
+			mailMessage.Body = messageReceived.PermissionsToString(messageReceived.RequestedPermissions, messageReceived.SenderName);
 
-            messageToSend.AddTo(messageBodyReceived.ReceiverEmail);
-			messageToSend.AddContent("text/html", messageBodyReceived.PermissionsToString(messageBodyReceived.RequestedPermissions, messageBodyReceived.SenderName));
-            messageToSend.SetFrom(messageBodyReceived.SenderName);
-            messageToSend.SetSubject("Permissions");
+			SmtpClient smtpClient = new SmtpClient();
+			smtpClient.Host = "smtp.gmail.com";
+			smtpClient.Port = 587;
+			smtpClient.UseDefaultCredentials = false;
+			smtpClient.Credentials = new NetworkCredential("", "");
+			smtpClient.EnableSsl = true;
+
+			smtpClient.Send(mailMessage);
 		}
 	}
 }

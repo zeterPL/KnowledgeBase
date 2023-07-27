20834b0b8a441c863a4c130c5d0af4ce24a35ced
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-07-27 12:36:05 +0200

    indentation

--- KnowledgeBase.Data/Configurations/ProjectConfiguration.cs
+++ KnowledgeBase.Data/Configurations/ProjectConfiguration.cs
@@ -6,27 +6,27 @@ namespace KnowledgeBase.Data.Configurations;
 
 public class ProjectConfiguration : IEntityTypeConfiguration<Project>
 {
-	public void Configure(EntityTypeBuilder<Project> builder)
-	{
-		builder.HasKey(p => p.Id);
+    public void Configure(EntityTypeBuilder<Project> builder)
+    {
+        builder.HasKey(p => p.Id);
 
-		builder.Property(p => p.Name)
-			.HasMaxLength(100);
+        builder.Property(p => p.Name)
+            .HasMaxLength(100);
 
-		builder.HasMany(p => p.Resources)
-			.WithOne(r => r.Project)
-			.HasForeignKey(r => r.ProjectId);
+        builder.HasMany(p => p.Resources)
+            .WithOne(r => r.Project)
+            .HasForeignKey(r => r.ProjectId);
 
-		builder.Property(p => p.StartDate)
-			.HasPrecision(3)
-			.HasDefaultValue(DateTime.Now);
-	
+        builder.Property(p => p.StartDate)
+            .HasPrecision(3)
+            .HasDefaultValue(DateTime.Now);
+
         builder.Property(p => p.Description)
             .HasMaxLength(500);
 
-		builder.HasOne(p => p.Owner)
-			.WithMany(p => p.ProjectsOwned)
-			.HasForeignKey(p => p.OwnerId)
-			.OnDelete(DeleteBehavior.NoAction);
-	}
+        builder.HasOne(p => p.Owner)
+            .WithMany(p => p.ProjectsOwned)
+            .HasForeignKey(p => p.OwnerId)
+            .OnDelete(DeleteBehavior.NoAction);
+    }
 }

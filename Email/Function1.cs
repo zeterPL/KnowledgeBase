194609be11c9bbae7edf1a4f8e55897a258c8653
PiotrDąbrowski <piotr.dabrowski2001@o2.pl>
2023-07-27 14:42:44 +0200

    SendGridMessage

--- Email/Function1.cs
+++ Email/Function1.cs
@@ -1,26 +1,34 @@
 using System;
 using System.Text;
+using System.Threading.Tasks;
 using Email.Models;
 using Microsoft.Azure.ServiceBus;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using SendGrid.Helpers.Mail;
 
 namespace Email
 {
     public class Function1
     {
         [FunctionName("Function1")]
-        public void Run([ServiceBusTrigger("email-queue", Connection = "connection")]string message, ILogger log)
+        public async Task Run([ServiceBusTrigger("email-queue", Connection = "connection")]string message, ILogger log)
         {
 
 			log.LogInformation($"C# ServiceBus queue trigger function processed message: {message}");
 
             var messageBodyReceived = JsonConvert.DeserializeObject<MessageModel>(message);
 
+            Console.WriteLine(messageBodyReceived.PermissionsToString(messageBodyReceived.RequestedPermissions, messageBodyReceived.SenderName));
             
+            var messageToSend = new SendGridMessage();
 
+            messageToSend.AddTo(messageBodyReceived.ReceiverEmail);
+			messageToSend.AddContent("text/html", messageBodyReceived.PermissionsToString(messageBodyReceived.RequestedPermissions, messageBodyReceived.SenderName));
+            messageToSend.SetFrom(messageBodyReceived.SenderName);
+            messageToSend.SetSubject("Permissions");
 		}
 	}
 }
--- Email/Models/MessageModel.cs
+++ Email/Models/MessageModel.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Email.Models
 {
@@ -12,5 +9,13 @@ namespace Email.Models
 		public string ReceiverEmail { get; set; }
 		public string ProjectName { get; set; }
 		public IEnumerable<string> RequestedPermissions { get; set; }
+
+
+		public string PermissionsToString(IEnumerable<string> permissions, string sender)
+		{
+			var permissionString = string.Join("\n", permissions.Select(permission => $" {permission}"));
+			return $"Dear Admin add for user named: {sender} permissions:\n{permissionString}";
+		}
+
 	}
 }
